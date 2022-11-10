8fbefbc7ac8c6318cbfe78ff40ffc2529a7201b6
Rafael Amora <rafael.amora.r19@gmail.com>
2022-11-10 04:19:07 -0300

    desenvolvimento 1.2

--- script.js
+++ script.js
@@ -7,22 +7,23 @@ $(".galeria").slick({
     nextArrow: false,
 });
 
-// Select button
+
+
 var button = document.getElementById('read_button');
 
-// Click Event
-button.addEventListener('click', function () {
-    // Select card
-    var card = document.querySelector('.card');
 
-    // Add/Remove Class Active
+button.addEventListener('click', function () {
+
+    var card = document.querySelector('#b1');
+
+
     card.classList.toggle('active');
 
     if (card.classList.contains('active')) {
-        // Change button text if has class active
+
         return button.textContent = 'Read less';
     }
 
-    // Change button text if hasn't class active
     button.textContent = 'Read more';
 });
+
